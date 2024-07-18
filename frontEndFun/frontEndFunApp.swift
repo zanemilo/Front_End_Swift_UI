87394bed0acd102ca4b9950de8b6d6a9f85a6928
zanedeso <zanemilodeso@gmail.com>
2024-07-18 17:02:34 -0400

    Initial Commit

--- frontEndFun/frontEndFunApp.swift
+++ frontEndFun/frontEndFunApp.swift
@@ -9,24 +9,10 @@ import SwiftUI
 import SwiftData
 
 @main
-struct frontEndFunApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
+struct frontEndFun: App {
     var body: some Scene {
-        WindowGroup {
+        WindowGroup{
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
